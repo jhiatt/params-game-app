68448eacc320aec6b0c26824b821111df235b3de
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-05-09 21:16:33 -0400

    now using url segments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/param_game", to: "games#games_method"
-  get "/guessnum", to: "games#getsnum_method"
+  get "/guessnum/:num", to: "games#getsnum_method"
 end
